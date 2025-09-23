c87237c0bf5130d76e4e05f8a5de1e02b01b4c18
David Rey <neisserrey@upeu.edu.pe>
2025-09-23 17:13:24 +0000

    fix: :rocket: Configuracion de deploy

--- main/settings/prod.py
+++ main/settings/prod.py
@@ -3,16 +3,30 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 INSTALLED_APPS = INSTALLED_LIBRARIES + INSTALLED_MODULES
+SESSION_COOKIE_AGE = 60 * 60 *2
+SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 
 DATABASES = {
-    'haberes_2025': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diris_haberes_2025',
+        'NAME': 'haberes_2015',
         'USER': 'root',
         'PASSWORD': 'Administrador@123',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
+    },
+    'haberes_2025': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'haberes_2025',
+        'USER': 'root',
+        'PASSWORD': 'Administrador@123',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
@@ -20,10 +34,10 @@ DATABASES = {
     },
     'haberes_2024': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diris_haberes_2024',
+        'NAME': 'haberes_2024',
         'USER': 'root',
         'PASSWORD': 'Administrador@123',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
@@ -31,10 +45,10 @@ DATABASES = {
     },
     'haberes_2023': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diris_haberes_2023',
+        'NAME': 'haberes_2023',
         'USER': 'root',
         'PASSWORD': 'Administrador@123',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
@@ -42,10 +56,10 @@ DATABASES = {
     },
     'haberes_2022': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diris_haberes_2022',
+        'NAME': 'haberes_2022',
         'USER': 'root',
         'PASSWORD': 'Administrador@123',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
@@ -53,16 +67,16 @@ DATABASES = {
     },
     'haberes_2021': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diris_haberes_2021',
+        'NAME': 'haberes_2021',
         'USER': 'root',
         'PASSWORD': 'Administrador@123',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
         }
     },
-    'default': {
+    'haberes_2020': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'diris_haberes_2020',
         'USER': 'root',
@@ -72,10 +86,10 @@ DATABASES = {
     },
     'haberes_2019': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diris_haberes_2019',
+        'NAME': 'haberes_2019',
         'USER': 'root',
         'PASSWORD': 'Administrador@123',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
@@ -83,10 +97,10 @@ DATABASES = {
     },
     'haberes_2018': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diris_haberes_2018',
+        'NAME': 'haberes_2018',
         'USER': 'root',
         'PASSWORD': 'Administrador@123',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
@@ -94,10 +108,10 @@ DATABASES = {
     },
     'haberes_2017': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diris_haberes_2017',
+        'NAME': 'haberes_2017',
         'USER': 'root',
         'PASSWORD': 'Administrador@123',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
@@ -105,10 +119,10 @@ DATABASES = {
     },
     'haberes_2016': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diris_haberes_2016',
+        'NAME': 'haberes_2016',
         'USER': 'root',
         'PASSWORD': 'Administrador@123',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
@@ -116,10 +130,10 @@ DATABASES = {
     },
     'haberes_2015': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diris_haberes_2015',
+        'NAME': 'haberes_2015',
         'USER': 'root',
         'PASSWORD': 'Administrador@123',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
@@ -128,10 +142,10 @@ DATABASES = {
 
     'haberes_2014': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diris_haberes_2014',
+        'NAME': 'haberes_2014',
         'USER': 'root',
         'PASSWORD': 'Administrador@123',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
@@ -139,10 +153,10 @@ DATABASES = {
     },
     'haberes_2013': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'diris_haberes_2013',
+        'NAME': 'haberes_2013',
         'USER': 'root',
         'PASSWORD': 'Administrador@123',
-        'HOST': '127.0.0.1',
+        'HOST': 'mysql',
         'PORT': '3306',
         'OPTIONS': {
             'sql_mode': 'traditional',
